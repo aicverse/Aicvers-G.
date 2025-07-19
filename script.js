5cbc3fdf0859472045710964927057376c7bc451
aicverse <fl1906p@gmail.com>
2025-07-19 11:20:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('aice').onclick = () => alert('🔔 AICE FIGHT STARTED!');
+document.getElementById('sig').onclick = () => alert('⚡ Signature Move!');
+document.getElementById('pin').onclick = () => alert('🤼 Attempting Pin!');
+document.getElementById('fin').onclick = () => alert('💥 Finisher!');
